1d8698a91b1156b28ed8a82f6c6cf49526cdd62d
elmik <elmik.karl@gmail.com>
2025-01-26 21:52:09 +0200

    Adding Updated, get and delete logic

--- Users/src/main/java/com/example/Users/controller/UserController.java
+++ Users/src/main/java/com/example/Users/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.example.Users.controller;
 
+import com.example.Users.constants.UsersConstants;
+import com.example.Users.dto.ResponseDto;
 import com.example.Users.dto.UserDto;
 import com.example.Users.dto.UserRegisterDto;
 import com.example.Users.service.impl.UserServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,41 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(users);
+    }
+
+    @GetMapping("/fetchById/{id}")
+    public ResponseEntity<UserDto> fetchUserById(@PathVariable String id) {
+        UserDto user = userService.fetchUserById(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(user);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateUserDetails(UserDto userDto) {
+        boolean isUpdated = userService.updatedUser(userDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(UsersConstants.STATUS_200, UsersConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(UsersConstants.STATUS_417, UsersConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseDto> deleteUser(@PathVariable String id) {
+        boolean isDeleted = userService.deletedUser(id);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(UsersConstants.STATUS_200, UsersConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(UsersConstants.STATUS_417, UsersConstants.MESSAGE_417_DELETE));
+        }
     }
 }
--- Users/src/main/java/com/example/Users/service/impl/UserServiceImpl.java
+++ Users/src/main/java/com/example/Users/service/impl/UserServiceImpl.java
@@ -3,16 +3,19 @@ package com.example.Users.service.impl;
 import com.example.Users.dto.UserDto;
 import com.example.Users.dto.UserRegisterDto;
 import com.example.Users.entity.User;
+import com.example.Users.exceptions.UserNotFoundException;
 import com.example.Users.mapper.UserMapper;
 import com.example.Users.repository.UserRepository;
 import com.example.Users.service.IUsersService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserServiceImpl implements IUsersService {
 
     private final UserRepository userRepository;
@@ -21,6 +24,39 @@ public class UserServiceImpl implements IUsersService {
     public List<UserDto> fetchUsers() {
         List<User> users = userRepository.findAll();
         if(users.isEmpty()) return List.of();
+        log.info("Fetching user all users");
         return users.stream().map(user -> UserMapper.mapToUsersDto(user, new UserDto())).toList();
+    }
+
+    @Override
+    public UserDto fetchUserById(String id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new UserNotFoundException("User", "id", id)
+        );
+        log.info("Fetching user with id: {}", id);
+        return UserMapper.mapToUsersDto(user, new UserDto());
+    }
+
+    @Override
+    public boolean updatedUser(UserDto userDto) {
+        boolean isUpdated = false;
+        User user = userRepository.findById(userDto.getId()).orElseThrow(
+                () -> new UserNotFoundException("User", "id", userDto.getId())
+        );
+        UserMapper.mapToUsers(userDto, user);
+        userRepository.save(user);
+        isUpdated = true;
+        log.info("Updated user with id: {}", userDto.getId());
+        return isUpdated;
+    }
+
+    @Override
+    public boolean deletedUser(String id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new UserNotFoundException("User", "id", id)
+        );
+        userRepository.deleteById(id);
+        log.info("Deleted user with id: {}", id);
+        return true;
     }
 }
