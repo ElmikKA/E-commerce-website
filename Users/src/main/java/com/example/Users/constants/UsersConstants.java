487a2ee5fe6d6e76bbab075c9827e60e49ba2ced
elmik <elmik.karl@gmail.com>
2025-01-26 20:50:52 +0200

    Exception Handling

---
+++ Users/src/main/java/com/example/Users/constants/UsersConstants.java
@@ -0,0 +1,15 @@
+package com.example.Users.constants;
+
+public class UsersConstants {
+    private UsersConstants(){
+        //No one can create object of this class
+    }
+
+    public static final String  STATUS_201 = "201";
+    public static final String  MESSAGE_201 = "User created successfully";
+    public static final String  STATUS_200 = "200";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+    public static final String  STATUS_417 = "417";
+    public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again.";
+    public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again.";
+}
