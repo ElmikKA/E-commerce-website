4aa309d6a5c3f6f8d57999c121ad2190aecb702f
elmik <elmik.karl@gmail.com>
2025-01-31 17:56:01 +0200

    MediaMapper

--- Media/src/main/java/com/example/Media/mapper/MediaMapper.java
+++ Media/src/main/java/com/example/Media/mapper/MediaMapper.java
@@ -8,14 +8,14 @@ public class MediaMapper {
     public static MediaDto mapToMediaDto(Media media, MediaDto mediaDto) {
         mediaDto.setId(media.getId());
         mediaDto.setImagedPath(media.getImagePath());
-        mediaDto.setUserId(media.getProductId());
+        mediaDto.setProductId(media.getProductId());
         return mediaDto;
     }
 
     public static Media mapToMedia(MediaDto mediaDto, Media media) {
         media.setId(mediaDto.getId());
         media.setImagePath(mediaDto.getImagedPath());
-        media.setProductId(mediaDto.getUserId());
+        media.setProductId(mediaDto.getProductId());
         return media;
     }
 }
