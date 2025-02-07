fee306291a55fdf94f93235c52d6f8f7ad0d2247
elmik <elmik.karl@gmail.com>
2025-02-07 11:39:42 +0200

    Added GET and PUT RESTApis

--- Products/src/main/java/com/example/Products/Controller/ProductController.java
+++ Products/src/main/java/com/example/Products/Controller/ProductController.java
@@ -1,15 +1,38 @@
 package com.example.Products.Controller;
 
+import com.example.Products.dto.ProductDto;
+import com.example.Products.service.IProductService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/products", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Slf4j
 public class ProductController {
 
-    @GetMapping("products")
-    public ResponseEntity<>
+    private final IProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        log.info("Fetching all products");
+        return ResponseEntity.ok(productService.fetchProducts());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ProductDto> createProduct(ProductDto productDto) {
+        log.info("Creating new product");
+        return ResponseEntity.ok(productService.createProduct(productDto));
+    }
+
+
 }
