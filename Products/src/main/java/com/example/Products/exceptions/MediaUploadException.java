7ce5aeb5741d381e7e35f971428d4189aca6e3a6
elmik <elmik.karl@gmail.com>
2025-02-12 00:23:58 +0200

    MediaUploadException in Products

---
+++ Products/src/main/java/com/example/Products/exceptions/MediaUploadException.java
@@ -0,0 +1,8 @@
+package com.example.Products.exceptions;
+
+public class MediaUploadException extends RuntimeException{
+
+    public MediaUploadException(String message) {
+        super(message);
+    }
+}
