b75aeeaee15d00d5c4fb3eea1ad1f14a1f098e6d
elmik <elmik.karl@gmail.com>
2025-04-07 18:41:35 +0300

    correlation id

--- Users/src/main/java/com/example/Users/service/IUserDetailsService.java
+++ Users/src/main/java/com/example/Users/service/IUserDetailsService.java
@@ -3,5 +3,5 @@ package com.example.Users.service;
 import com.example.Users.dto.UserDetailsDto;
 
 public interface IUserDetailsService {
-    UserDetailsDto fetchUserDetails(String userId);
+    UserDetailsDto fetchUserDetails(String userId, String correlationId);
 }
